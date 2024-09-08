1e8ffafa2cacefbb579f5fbb623f6d71a9c8d300
Uzair Rana <116478578+UzairRan@users.noreply.github.com>
2024-09-08 08:53:09 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('addBtn').addEventListener('click', function() {
+    let task = document.getElementById('todoInput').value;
+    if (task) {
+        let listItem = document.createElement('li');
+        listItem.textContent = task;
+        document.getElementById('todoList').appendChild(listItem);
+        document.getElementById('todoInput').value = ''; // Clear input
+    }
+});
